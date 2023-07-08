4ee4c3609667f28ac319c3e47112195ec247fbb0
pasinsiri <pasin_plp0206@hotmail.com>
2023-07-08 20:23:15 +0700

    remove arg and use system timestamp instead

--- autogen.py
+++ autogen.py
@@ -1,21 +1,14 @@
 import logging
 import datetime as dt
+import time
 import os
 import sys
 from functions.generator import RandomNumberGenerator
 
 logging.basicConfig(level=logging.INFO)
 
-if len(sys.argv) <= 1:
-    raise AttributeError('No arguments found')
-logging.info(f"Arguments: {', '.join(sys.argv[1:])}")
-
-curr_date = sys.argv[1]
-if isinstance(curr_date, str):
-    curr_date = dt.datetime.strftime(curr_date, '%Y-%m-%d %H:%m:%s')
-elif isinstance(curr_date, dt.date):
-    curr_date = dt.datetime(curr_date)
-
+curr_time = dt.datetime.now()
+curr_date = curr_time.date()
 logging.info(f"Running at {curr_date}")
 rng = RandomNumberGenerator(n=5)
 numbers = rng.generate_random_numbers(lower_bound=1, upper_bound=100)
