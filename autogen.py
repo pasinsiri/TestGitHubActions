df31396f664b3e3559edb7f6094420b7bbf60af5
pasinsiri <pasin_plp0206@hotmail.com>
2023-07-08 19:08:49 +0700

    add cases for date input transform

--- autogen.py
+++ autogen.py
@@ -10,7 +10,12 @@ if len(sys.argv) <= 1:
     raise AttributeError('No arguments found')
 
 curr_date = sys.argv[1]
-logging.info(f"Running at {dt.datetime.strftime(curr_date, '%Y-%m-%d %H:%m:%s')}")
+if isinstance(curr_date, str):
+    curr_date = dt.datetime.strftime(curr_date, '%Y-%m-%d %H:%m:%s')
+elif isinstance(curr_date, dt.date):
+    curr_date = dt.datetime(curr_date)
+
+logging.info(f"Running at {curr_date}")
 rng = RandomNumberGenerator(n=5)
 numbers = rng.generate_random_numbers(lower_bound=1, upper_bound=100)
 for i in range(len(numbers)):
