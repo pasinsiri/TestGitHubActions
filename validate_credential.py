bdaf76f59440db113f511809513f7221155936c3
pasinsiri <pasin_plp0206@hotmail.com>
2023-07-08 23:09:07 +0700

    add mapper check

--- validate_credential.py
+++ validate_credential.py
@@ -5,3 +5,10 @@ args = sys.argv[1:]
 
 mapper_raw = json.load(open('./keys/mapper.json', 'r'))
 mapper = {sub_v:k for k, v in mapper_raw.items() for sub_v in v}
+
+for a in args:
+    a = a.lower()
+    if a not in mapper:
+        print(f'Argument {a} not found')
+    else:
+        print(f'Argument {a} falls in the category of {mapper[a]}')
