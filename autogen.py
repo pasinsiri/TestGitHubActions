f3a9f101a3b62ac91de17aa7c722fae2887bd239
pasinsiri <pasin_plp0206@hotmail.com>
2023-07-08 15:55:30 +0700

    create random number generator script

--- autogen.py
+++ autogen.py
@@ -1,4 +1,9 @@
-import random 
+import logging
 from functions.generator import RandomNumberGenerator
 
-rng = RandomNumberGenerator()
+rng = RandomNumberGenerator(n=5)
+numbers = rng.generate_random_numbers(lower_bound=1, upper_bound=100)
+for i in range(numbers):
+    logging.info(f'Number in order {i} is {numbers[i]}')
+
+logging.info('Iteration completed!')
