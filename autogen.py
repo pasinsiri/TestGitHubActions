e2775826c575beac0c0414bd2d8fadadde4dd518
pasinsiri <pasin_plp0206@hotmail.com>
2023-07-08 16:03:53 +0700

    add sys.argv

--- autogen.py
+++ autogen.py
@@ -1,6 +1,14 @@
 import logging
+import os
+import sys
 from functions.generator import RandomNumberGenerator
 
+
+if len(sys.argv) == 0:
+    raise AttributeError('No arguments found')
+
+curr_date = sys.argv[0]
+logging.info(f'Running at ')
 rng = RandomNumberGenerator(n=5)
 numbers = rng.generate_random_numbers(lower_bound=1, upper_bound=100)
 for i in range(numbers):
