5cfbc840839418ec74f5ae13d77e138df9c1b0f3
pasinsiri <pasin_plp0206@hotmail.com>
2023-07-11 22:43:00 +0700

    debug argparse

--- dropbox.py
+++ dropbox.py
@@ -7,14 +7,14 @@ parser = argparse.ArgumentParser(
     description='Get Dropbox credentials',
     epilog='The credential is required for the Dropbox connector'
 )
-parser.add_argument('-dropbox_key', '--dropbox_key')
-parser.add_argument('-dropbox_secret', '--dropbox_token')
-parser.add_argument('-dropbox_token', '--dropbox_token')
+parser.add_argument('-k', '--key')
+parser.add_argument('-sc', '--secret')
+parser.add_argument('-tk', '--token')
 args = parser.parse_args()
 
 # * extract credentials
-key = parser.dropbox_key
-secret = parser.dropbox_secret
-token = parser.dropbox_token
+key = args.key
+secret = args.secret
+token = args.token
 
 print(f'Key = {key}, Secret = {secret}, Token = {token}')
