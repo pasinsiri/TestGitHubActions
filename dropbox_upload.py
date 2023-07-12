006108c960b3860410416720734bf63158fcab43
pasinsiri <pasin_plp0206@hotmail.com>
2023-07-12 21:42:43 +0700

    rename dropbox_upload.py and generate sample data

---
+++ dropbox_upload.py
@@ -0,0 +1,34 @@
+import argparse
+import numpy as np
+import pandas as pd
+import dropbox
+from io import BytesIO, StringIO
+
+# * retrieve dropbox credentials
+parser = argparse.ArgumentParser(
+    prog='DropboxAuthenticator',
+    description='Get Dropbox credentials',
+    epilog='The credential is required for the Dropbox connector'
+)
+parser.add_argument('-k', '--key')
+parser.add_argument('-sc', '--secret')
+parser.add_argument('-tk', '--token')
+args = parser.parse_args()
+
+# * extract credentials
+key = args.key
+secret = args.secret
+token = args.token
+
+# * generate random data
+sample_array = np.array(
+    [['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
+    np.random.randint(low=10, high=20, size=10),
+    np.random.randint(low=200, high=500, size=10)]
+).T
+sample_df = pd.DataFrame(sample_array, columns=['key', 'value1', 'value2'])
+
+# * connect to dropbox and upload data
+dbx = dropbox.Dropbox(token)
+df_io = sample_df.to_csv(index=False).encode()
+dbx.files_upload(df_io, '/Test GitHub Actions/sample_data.csv', mode=dropbox.files.WriteMode.overwrite)
